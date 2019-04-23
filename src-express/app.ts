27315a81d6b7b7bb574dd0a071f494dd1372f7cb
Anh Nhan Nguyen <anhnhan@outlook.com>
2019-04-24 00:44:54 +0200

    build discord formatter

--- src-express/app.ts
+++ src-express/app.ts
@@ -1,5 +1,7 @@
 import * as express from 'express';
 import * as Twitter from 'twit';
+import * as cors from 'cors';
+import * as bodyParser from 'body-parser';
 
 import { twitterCredentials } from './config';
 
@@ -8,6 +10,22 @@ const port = 3000;
 
 const twitterClient = new Twitter(twitterCredentials);
 
+app.use(cors());
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/statuses/show/:id', (req, res) => {
+  const tweetId: string = req.params.id;
+  if (!tweetId || !tweetId.length || !/^\d+$/.test(tweetId)) {
+    res.statusCode = 400;
+    res.send('invalid tweet id');
+    return;
+  }
+  twitterClient.get(`statuses/show/${tweetId}`)
+    .then(apiResponse => res.send(apiResponse.data))
+    .catch((error) => console.error(error))
+    ;
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ReturnStatement } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -9,43 +10,96 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'sns-discord-formatter';
 
+  private apiBase = '//localhost:3000';
+
   public inProgress = false;
 
-  public linkInput: string;
+  public linkInput: string = 'https://twitter.com/JYPETWICE/status/1120341497742626816';
 
   public notRecognized = false;
 
   public output = 'Example output...';
 
+  public guessDate: string;
+  public tweetText: string;
+  public tweetUrl: string;
+  public authorName: string;
+  public mediaLinks: string[] = [];
+  public urls: string[] = [];
+  public truncated = false;
+
   constructor(
     private $http: HttpClient,
-  ) {}
+  ) { }
+
+  onInit() {
+    this.linkInputUpdate(this.linkInput);
+  }
 
   linkInputUpdate(event: string) {
     this.notRecognized = false;
+    this.reset();
     this.output = '';
 
     const trimmedInput = this.linkInput.trim();
-    const isTwitterTweetUrl = /^https:\/\/twitter.com\/[^/]+\/status\/\d+(\?s=\d+)?$/;
+    const isTwitterTweetUrl = /^((\d{16,})|https:\/\/twitter.com\/[^/]+\/status\/\d+(\?s=\d+)?)$/;
     if (isTwitterTweetUrl.test(trimmedInput)) {
-      return this.handleTwitter(trimmedInput);
+      const matches = trimmedInput.match(/^((\d+)|https:\/\/twitter.com\/[^/]+\/status\/(\d+)(?:\?s=\d+)?)$/);
+      const tweetId = matches[2] || matches[3];
+      return this.handleTwitterTweet(tweetId);
     }
 
     // no further handlers
     if (trimmedInput.length !== 0) {
       this.notRecognized = true;
     }
-    this.output = 'Example output...';
+    this.reset();
   }
 
-  handleTwitter(url: string) {
-    const normalizedUrl = url.replace(/(\?s=\d+|\s+)+$/, '');
+  reset() {
+    this.output = 'Example output...';
+    this.guessDate = undefined;
+    this.tweetText = undefined;
+    this.tweetUrl = undefined;
+    this.authorName = undefined;
+    this.mediaLinks = [];
+    this.urls = [];
+    this.truncated = false;
+  }
+
+  handleTwitterTweet(tweetId: string) {
+    const url = `${this.apiBase}/statuses/show/${tweetId}`;
     this.inProgress = true;
-    this.$http.get(normalizedUrl, {
-      responseType: 'text',
-    }).subscribe(response => {
+    this.$http.get(url).subscribe((tweet: any) => {
+      if (tweet.truncated) {
+        this.truncated = true;
+        this.inProgress = false;
+        return;
+      }
+      this.authorName = tweet.user.name;
+      this.tweetText = (tweet.text as string)
+        .replace(/https:\/\/t\.co\/\w+/g, '')
+        .replace(/(\s)+/gm, ' ')
+        .trim()
+        ;
+      this.tweetUrl = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`;
+      if (tweet.extended_entities) {
+        this.mediaLinks = tweet.extended_entities.media
+          .map((media: { type: string; expanded_url: any; media_url_https: any; }) =>
+            media.type === 'video' || media.type === 'animated_gif' ? media.expanded_url : media.media_url_https);
+      }
+      this.urls = tweet.entities.urls.map((url: any) => url.expanded_url);
+
+      this.output = this.renderTweet();
+
       this.inProgress = false;
-      console.log(response);
     });
+  }
+
+  renderTweet() {
+    return `\`${this.tweetText}\`
+cr. @${this.authorName} <${this.tweetUrl}>
+${this.mediaLinks.join('\n')}
+${this.urls.join('\n')}`;
   }
 }
