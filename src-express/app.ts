8541a5e075dc4e186609709be2f839e7eee3f3bc
Anh Nhan Nguyen <anhnhan@outlook.com>
2019-04-29 10:52:32 +0000

    oops did 404 the wrong way

--- src-express/app.ts
+++ src-express/app.ts
@@ -13,8 +13,6 @@ const twitterClient = new Twitter(twitterCredentials);
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('This API is not meant to be visited by a browser.'));
-
 app.use(express.static(__dirname + '../../sns-discord-formatter'));
 
 app.get('/statuses/show/:id', (req, res) => {
@@ -31,3 +29,7 @@ app.get('/statuses/show/:id', (req, res) => {
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.use(function(req, res, next) {
+  res.status(404).send('This API is not meant to be visited by a browser.');
+});
