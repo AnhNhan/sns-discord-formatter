44b5bb26255f30033d463aa36eac12ed5d0e85fb
Anh Nhan Nguyen <anhnhan@outlook.com>
2019-05-04 22:46:47 +0200

    throw out api server stuff
    ref #1

--- src-express/config.ts
+++
@@ -1,7 +0,0 @@
-export
-const twitterCredentials = {
-  consumer_key: process.env.SECRET_CONSUMER_KEY || 'CONSUMER_KEY',
-  consumer_secret: process.env.SECRET_CONSUMER_SECRET || 'CONSUMER_SECRET',
-  access_token: process.env.SECRET_ACCESS_TOKEN || 'ACCESS_TOKEN',
-  access_token_secret: process.env.SECRET_ACCESS_TOKEN_SECRET || 'ACCESS_TOKEN_SECRET'
-};
